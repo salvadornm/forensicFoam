723c9ec392b35703146dc3f22f2229f8a4d0a23c
Salvador <s.navarro@imperial.ac.uk>
2024-02-08 19:02:16 +0000

    Example added

---
+++ examples/Lousiana/numbers_simulation.py
@@ -0,0 +1,79 @@
+import math 
+
+# constants
+sigma = 5.67e-8 # si units
+Runi = 8.314 #J/mol K
+kboltzmann =1.38066e-23 #J/K 
+cm2m = 100.0#
+T0 = 273.15 #
+NA = 6.023e23 # Avogadro
+F = 96485.3 # C/mol
+P0 = 103125  # 1atm
+g =  9.81    # garvity
+
+kappa = 0.41
+Cmu  = 0.09
+
+print('Lousiana numbers 2023')
+
+print('  LOG WALL')
+
+H=2000  # Height
+
+Knts2ms = 0.514444 # 1 knts = 0.51444 m/s
+
+#-----------------
+Uref = 5  # Knts
+Uref = 5*Knts2ms # reference velocity  (measured)
+zref = 9.1 # reference height
+z0 = 0.5  # Surface roughness height 
+zg =  0.0
+#-----------------
+
+print(' [OpenFOAM] Uref for Uref=',Uref)
+
+num = zref + z0
+Ustar = kappa *Uref/math.log(num/z0)
+print('U* (friction velocity)',Ustar, '[m/s]')
+
+
+num = H - zg + z0
+ln1 = math.log(num/z0)
+print('U/U* ',ln1/kappa, " at z=",H)
+U = Ustar*ln1/kappa
+print('U  ',U, "[m/s] at z=",H)
+print(' [OpenFOAM] number or top BC for 0/U=',U)
+
+k = Ustar*Ustar/math.sqrt(Cmu)
+
+print("k (turbulent kinetic energy) ",k, "[m2/s2] ")
+
+print(' [OpenFOAM] number on inlet  for 0/k=',k)
+
+
+# P=P0*exp(-z/z0) atmospheric drop
+M = 0.028964 # kg/mol
+T = 298.15
+P = P0*math.exp(-g*M*H/(Runi*T))
+
+print("P = ",P, "[Pa] at z= ",H," m")
+
+rho0 = P0*M/(Runi*T)
+rho  = P*M/(Runi*T)
+
+print("rho0 = ",rho0, "[Pa] at z= ",0," m")
+print("rho = ",rho, "[Pa] at z= ",H," m")
+
+
+print(" ---------------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
